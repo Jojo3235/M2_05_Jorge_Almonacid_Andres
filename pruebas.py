2be6cac5624ab371aebac63495675e15376ea840
Jojo3235 <joralmand@gmail.com>
2022-10-12 00:20:29 +0200

    empezar ejercicio3 y algunos cambios de funciones

---
+++ pruebas.py
@@ -0,0 +1,5 @@
+ #Ejercicio 3
+
+palabra=input("Introduce una oració, frase o palabra para contar cuantas veces tiene una letra: ")
+letra=input("Introduce que caracter quieres que cuente: ")
+print(palabra.count(letra))
